23b91f420d0788cab99cf92a8dfd338b19721bc3
Dk-code15 <71776586+Dk-code15@users.noreply.github.com>
2020-10-01 20:39:31 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,12 +14,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston
 # MA 02110-1301, USA.
 
 
-import wx
-import CalcFrame
+import wx  # Importing wx
+import CalcFrame  # importing calcframe
 class App(wx.App):
 	def OnInit(self):
 		calc = CalcFrame.Calculator((250,300))
